161caedacf0240602c30770d9078871b598ca2f5
Brett Valantine <brett.valantine@angel.com>
2022-09-27 10:58:53 -0600

    ideas around using IO Data

--- lib/hls/serializer.ex
+++ lib/hls/serializer.ex
@@ -1,27 +1,45 @@
 defmodule HLS.Serializer do
+
+
   def run(%HLS.Manifest{} = manifest) do
-    {txt, _} =
-      {"", manifest}
+    # build up m3u8 as IO Data.  Could be done as an eex template too.
+    {iodata, _} =
+      {[], manifest}
       |> add_basic_tags()
       |> add_version()
       |> add_independent_segments()
+      |> reverse_data()
 
-    txt
+    # adds newlines to the iodata above by using IO.puts.
+    {:ok, pid} = StringIO.open("")
+    Enum.each(iodata, fn line -> IO.puts(pid, line) end)
+    
+    # return a string from StringIO
+    {:ok, {_input_buffer, output_buffer}} = StringIO.close(pid)
+    output_buffer
   end
 
-  defp add_basic_tags({txt, manifest}) do
-    {txt <> "#EXTM3U\n", manifest}
+  defp add_basic_tags({data, manifest}) do
+    {[ "#EXTM3U" | data], manifest}
   end
 
-  defp add_version({txt, %{version: nil}} = tuple), do: tuple
+  defp add_version({_data, %{version: nil}} = tuple), do: tuple
 
-  defp add_version({txt, %{version: ver} = manifest}) do
-    {txt <> "#EXT-X-VERSION:#{ver}\n", manifest}
+  defp add_version({data, %{version: ver} = manifest}) do
+    {[["#EXT-X-VERSION:", to_string(ver)] | data], manifest}
   end
 
-  defp add_independent_segments({txt, %{independent_segments: true} = manifest}) do
-    {txt <> "#EXT-X-INDEPENDENT-SEGMENTS", manifest}
+  defp add_independent_segments({data, %{independent_segments: true} = manifest}) do
+    {["#EXT-X-INDEPENDENT-SEGMENTS" | data], manifest}
   end
 
-  defp add_independent_segments({txt, %{independent_segments: nil}} = tuple), do: tuple
+  defp add_independent_segments({_data, %{independent_segments: nil}} = tuple), do: tuple
+
+  defp reverse_data({data, manifest}), do: {Enum.reverse(data), manifest}
+end
+
+defimpl String.Chars, for: HLS.Manifest do
+  def to_string(manifest) do
+    HLS.Serializer.run(manifest)
+  end
 end
